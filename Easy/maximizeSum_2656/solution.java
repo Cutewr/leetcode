5e8b701dce6d8fa6654daaaf7ed0a2452157f856
Cutewr <2574303446@qq.com>
2023-11-15 20:43:24 +0800

    11.15

---
+++ Easy/maximizeSum_2656/solution.java
@@ -0,0 +1,21 @@
+package Easy.maximizeSum_2656;
+
+class solution {
+    public static void main(String[] args) {
+        int []nums={5,5,5};
+        int result=maximizeSum(nums,2);
+        System.out.println(result);
+    }
+    public static int maximizeSum(int[] nums, int k) {
+        int len=nums.length;
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>max) {
+                max=nums[i];
+            }
+        }
+        int add=k*(k-1)/2;
+        return(max*k+add);
+    }
+
+}
