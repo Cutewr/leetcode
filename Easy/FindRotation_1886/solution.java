d620e89f8c3c077b122133126883ed1697c20c05
Cutewr <2574303446@qq.com>
2023-12-06 14:36:50 +0800

    2023.12.6

---
+++ Easy/FindRotation_1886/solution.java
@@ -0,0 +1,10 @@
+package Easy.FindRotation_1886;
+
+public class solution {
+    public static void main(String[] args) {
+        
+    }
+    public boolean findRotation(int[][] mat, int[][] target) {
+
+    }
+}
