70ccdd9ee2340be906cbe36a497895b375274afc
Cutewr <2574303446@qq.com>
2023-12-06 18:27:37 +0800

    2023.12.6

---
+++ Easy/Merge_88/solution.java
@@ -0,0 +1,14 @@
+package Easy.Merge_88;
+
+public class solution {
+    public static void main(String[] args) {
+        int []nums1={};
+        int m=nums1.length;
+        int []nums2={};
+        int n=nums2.length;
+        merge(nums1,n,nums2,m);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    
+    }
+}
