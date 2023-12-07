b2ea2f4d65c2cdd0872c4f4a573015b71243c11f
Cutewr <2574303446@qq.com>
2023-12-07 14:47:36 +0800

    2023.12.7

--- Easy/MajorityElement_169/solution3.java
+++ Easy/MajorityElement_169/solution3.java
@@ -2,11 +2,27 @@ package Easy.MajorityElement_169;
 
 public class solution3 {
     public static void main(String[] args) {
-        
+        int []nums={1,1,2,1,2,3,3,3,3,3};
+        int res=majorityElement(nums);
+        System.out.println(res);
     }
     //摩尔投票
     public static int majorityElement(int[] nums) {
-
-        return 0;
+        int m=0,cnt=0;
+        for(int num:nums){
+            if (cnt==0) {
+                m=num;
+                cnt++;
+            }
+            else{
+                if (m!=num) {
+                    cnt--;
+                }
+                if (m==num) {
+                    cnt++;
+                }
+            }
+        }
+        return m;
     }
 }
---
+++ Medium/CanJump_55/solution.java
@@ -0,0 +1,11 @@
+package Medium.CanJump_55;
+
+public class solution {
+    public static void main(String[] args) {
+        
+    }
+    public boolean canJump(int[] nums) {
+        
+        return true;
+    }
+}
