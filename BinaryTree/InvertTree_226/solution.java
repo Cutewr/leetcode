f2c79ac9d326a37f503c6ca3b74e2efcfa6ba8d9
Cutewr <2574303446@qq.com>
2023-12-13 13:51:52 +0800

    2023.12.13

--- BinaryTree/InvertTree_226/solution.java
+++ BinaryTree/InvertTree_226/solution.java
@@ -4,16 +4,36 @@ import BinaryTree.TreeNode;
 
 public class solution {
     public static void main(String[] args) {
-        
+        TreeNode root=new TreeNode(3,new TreeNode(9),new TreeNode(20,new TreeNode(15),new TreeNode(7)));
+        TreeNode res=new TreeNode();
+        res=invertTree(root);
+        dfsOut(res);
+        System.out.println();
+        dfsOut(root);
     }
+
     public static TreeNode invertTree(TreeNode root) {
-        if(root==null)  return null;
-        
+        invert(root);
         return root;
     }
+
+    public static void invert(TreeNode root){
+        if(root==null)  return;
+        change(root.left, root.right);
+        invert(root.left);
+        invert(root.right);
+    }
+
     public static void change(TreeNode l,TreeNode r){
-        int temp=l.val;
-        l.val=r.val;
-        r.val=temp;
+        TreeNode temp=l;
+        l=r;
+        r=temp;
+    }
+
+    public static void dfsOut(TreeNode root){
+        if (root==null) {System.out.print("null "); return;}
+        System.out.print(root.val+" ");
+        dfsOut(root.left);
+        dfsOut(root.right);
     }
 }
