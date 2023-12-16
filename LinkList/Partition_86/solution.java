589510bf5c682d6afa4f71072513cc0d6589250c
Cutewr <2574303446@qq.com>
2023-12-16 20:28:52 +0800

    2023.12.16

---
+++ LinkList/Partition_86/solution.java
@@ -0,0 +1,14 @@
+package LinkList.Partition_86;
+
+import LinkList.ListNode;
+
+public class solution {
+    public static void main(String[] args) {
+        
+    }
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummyNode=new ListNode(0,head);
+
+        return dummyNode.next;
+    }
+}
